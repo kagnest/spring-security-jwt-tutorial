1b4029ea23a4a542c2e402e5a0a917ee7076e131
Agnes Kadlot <agnes.kadlot@gmail.com>
2020-03-25 16:15:30 +0100

    Complete asssignment

---
+++ src/main/java/com/example/demo/security/SecurityConfig.java
@@ -0,0 +1,50 @@
+package com.example.demo.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final JwtTokenServices jwtTokenServices;
+
+    public SecurityConfig(JwtTokenServices jwtTokenServices) {
+        this.jwtTokenServices = jwtTokenServices;
+    }
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+//        super.configure(http);
+        http
+                .httpBasic().disable()
+                .csrf().disable()
+
+            .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeRequests()
+                .antMatchers("/auth/signin")
+                .permitAll()
+                .antMatchers(HttpMethod.GET, "/vehicles/**")
+                .authenticated()
+                .antMatchers(HttpMethod.DELETE, "/vehicles/**")
+                .hasRole("ADMIN")
+                .antMatchers(HttpMethod.GET, "/me").authenticated()
+                .anyRequest().denyAll()
+                .and()
+                .addFilterBefore(new JwtTokenFilter(jwtTokenServices), UsernamePasswordAuthenticationFilter.class);
+    }
+}
